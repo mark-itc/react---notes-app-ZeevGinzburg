08945593f0a0c270e714f92caef71debb8080ff6
Zeev Ginzburg <109742174+ZeevGinzburg@users.noreply.github.com>
2022-11-13 15:40:35 +0200

    test2

--- zeev-ginzburg-react-1-notes-app/src/App.js
+++ zeev-ginzburg-react-1-notes-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AddButton from './components/add-button';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           TEST!!
+          TEST2!!!
         </a>
       </header>
     </div>
---
+++ zeev-ginzburg-react-1-notes-app/src/components/add-button.jsx
@@ -0,0 +1,5 @@
+function AddButton(){
+
+}
+
+export default AddButton;
